e4c775524531a7ab43e11b3e2363d35a747ec8d8
jewelcse <jewelcse045@gmail.com>
2023-03-31 10:14:45 +0600

    resolved CrossOrigin issue

--- src/main/java/com/image/uploader/service/controller/ImageUploadController.java
+++ src/main/java/com/image/uploader/service/controller/ImageUploadController.java
@@ -23,6 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/images")
 @AllArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ImageUploadController {
 
     private final ImageService imageService;
